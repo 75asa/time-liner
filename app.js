98ea89e0a09af8ec892f09c4731c8a715b2fae1f
Asato Nago <nago@tam-bourine.co.jp>
2019-11-05 00:11:09 +0900

    🎉

---
+++ app.js
@@ -0,0 +1,27 @@
+const env = require('dotenv').config();
+const { App } = require('@slack/bolt');
+const util = require('util');
+
+// Initializes your app with your bot token and signing secret
+const app = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET
+});
+
+// Listens to incoming messages that contain "hello"
+app.message('hello', ({ message, say }) => {
+  // say() sends a message to the channel where the event was triggered
+  say(`おっっす:hellokitty: <@${message.user}>\n*調子どうよ？*:ago:`);
+});
+
+(async () => {
+  // Start your app
+  await app.start(process.env.PORT || 3000);
+
+  console.log('⚡️ Bolt app is running!');
+})();
+
+// RequestURL
+// https://d-tambourine-bolt-tutorial.com/slack/events
+// official: e.g.
+// https://8e8ec2d7.ngrok.io/slack/events
