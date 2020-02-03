57966652dac88172da5512ec7431c6399c8c561c
Asato Nago <nago@tam-bourine.co.jp>
2020-02-03 23:03:02 +0900

    feat:

--- app.js
+++ app.js
@@ -1,23 +1,25 @@
-const env = require('dotenv').config();
+require('dotenv').config();
 const { App } = require('@slack/bolt');
-const bodyParser = require('body-parser');
-const util = require('util');
 
 // Initializes your app with your bot token and signing secret
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
-  signingSecret: process.env.SLACK_SIGNING_SECRET
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+  channel: process.env.CHANNEL_ID
 });
 
-app.post("/slack/events", (req, res) => {
-  return res.status(200).json({ 'challenge': req.body.challenge });
-});
-
-// Listens to incoming messages that contain "hello"
-app.message('hello', ({ message, say }) => {
-  // console.log({ message })
-  // say('hoge')
-  say(`おっっす <@${message.user}>`);
+app.event('message', async ({ event, context }) => {
+  try {
+    const result = await app.client.chat.postMessage({
+      token: App.token,
+      channel: App.channel,
+      text: `>> ${message.text}`
+    });
+    console.log(result);
+  }
+  catch (error) {
+    console.error(error);
+  }
 });
 
 (async () => {
@@ -29,5 +31,3 @@ app.message('hello', ({ message, say }) => {
 
 // RequestURL
 // https://d-tambourine-bolt-tutorial.com/slack/events
-// official: e.g.
-// https://8e8ec2d7.ngrok.io/slack/events
