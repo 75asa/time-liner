9d0e140166e0df91b86cb87ed7ea66b76362f25a
tk-ozawa <ozawa.shotaro0114@gmail.com>
2020-10-27 13:01:22 +0900

    add: MiddlewareParam

--- src/bolt.interface.ts
+++ src/bolt.interface.ts
@@ -1,8 +1,15 @@
-import { Context, MessageEvent } from "@slack/bolt";
+import { Context, MessageEvent, NextFn } from "@slack/bolt";
 import { WebClient } from "@slack/web-api";
 
 export interface MessageEventParam {
   message: MessageEvent;
   context: Context;
   client?: WebClient;
+}
+
+export interface MiddlewareParam {
+  next: NextFn;
+  client?: WebClient;
+  message?: MessageEvent;
+  context?: Context;
 }
