1c74e979f27ada0c7261522a70342e93b83249b7
tk-ozawa <ozawa.shotaro0114@gmail.com>
2020-10-27 11:43:36 +0900

    update: 不要なregexの削除

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,6 @@ import { ChatPostMessageArguments } from "@slack/web-api";
 import dotenv from "dotenv";
 import * as middleware from "./customMiddleware";
 import * as blocKit from "./block";
-import { MessageEventParam } from "./bolt.interface";
 
 dotenv.config();
 
@@ -25,24 +24,60 @@ app.use(middleware.getTeamInfo);
 app.use(middleware.addUsersInfoContext);
 app.use(middleware.getFileInfo);
 
-app.message(
-  middleware.getChannelInfo,
-  /^(.*)/ as any,
-  async ({ client, context, message }) => {
-    const msgOption: ChatPostMessageArguments = {
-      token: client.token,
-      channel: process.env.CHANNEL_NAME,
-      text: message.text,
-      unfurl_links: true,
-      link_names: true,
-      unfurl_media: true,
-      icon_url: context.profile.image_original,
-      username: context.profile.display_name || context.profile.real_name,
-      blocks: await blocKit.dealBlock({ context, message }),
-    };
+app.message(middleware.getChannelInfo, async ({ client, context, message }) => {
+  const msgOption: ChatPostMessageArguments = {
+    token: client.token,
+    channel: process.env.CHANNEL_NAME,
+    text: message.text,
+    unfurl_links: true,
+    link_names: true,
+    unfurl_media: true,
+    icon_url: context.profile.image_original,
+    username: context.profile.display_name || context.profile.real_name,
+    blocks: await blocKit.dealBlock({ context, message }),
+  };
 
-    console.log("1回目", JSON.stringify(msgOption, null, 4));
+  console.log("1回目", JSON.stringify(msgOption, null, 4));
 
+  await app.client.chat
+    .postMessage(msgOption)
+    .then((res) => {
+      if (res.ok) console.log("msg: ok ✅");
+    })
+    .catch((err) => {
+      console.error({ err });
+      console.log(err.data.response_metadata);
+    });
+
+  console.log(JSON.stringify(context.files, null, 4));
+
+  if (context.files && context.files.files.length) {
+    // snippet, POST がある場合は blocks があると送信できないので本文投稿後に再度ファイルだけ投稿
+    // icon_url, usernameもpayloadに存在するとattachmentの展開がされないので削除
+    delete msgOption.blocks;
+    delete msgOption.icon_url;
+    delete msgOption.username;
+    await Promise.all(
+      context.files.files.map(async (file) => {
+        return await new Promise<string>((resolve, reject) => {
+          if (file.permalink) {
+            resolve(file.permalink);
+          } else {
+            reject("error");
+          }
+        });
+      })
+    )
+      .then(async (result) => {
+        // console.log({ result });
+        await result.forEach((value) => {
+          msgOption.text = value as string;
+        });
+      })
+      .catch((e) => {
+        console.log({ e });
+      });
+    console.log("2回目", JSON.stringify(msgOption, null, 4));
     await app.client.chat
       .postMessage(msgOption)
       .then((res) => {
@@ -52,48 +87,8 @@ app.message(
         console.error({ err });
         console.log(err.data.response_metadata);
       });
-
-    console.log(JSON.stringify(context.files, null, 4));
-
-    if (context.files && context.files.files.length) {
-      // snippet, POST がある場合は blocks があると送信できないので本文投稿後に再度ファイルだけ投稿
-      // icon_url, usernameもpayloadに存在するとattachmentの展開がされないので削除
-      delete msgOption.blocks;
-      delete msgOption.icon_url;
-      delete msgOption.username;
-      await Promise.all(
-        context.files.files.map(async (file) => {
-          return await new Promise<string>((resolve, reject) => {
-            if (file.permalink) {
-              resolve(file.permalink);
-            } else {
-              reject("error");
-            }
-          });
-        })
-      )
-        .then(async (result) => {
-          // console.log({ result });
-          await result.forEach((value) => {
-            msgOption.text = value as string;
-          });
-        })
-        .catch((e) => {
-          console.log({ e });
-        });
-      console.log("2回目", JSON.stringify(msgOption, null, 4));
-      await app.client.chat
-        .postMessage(msgOption)
-        .then((res) => {
-          if (res.ok) console.log("msg: ok ✅");
-        })
-        .catch((err) => {
-          console.error({ err });
-          console.log(err.data.response_metadata);
-        });
-    }
   }
-);
+});
 
 (async () => {
   // Start your app
