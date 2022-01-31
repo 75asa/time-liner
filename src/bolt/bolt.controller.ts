4bc8bcc3afc95cc72bdb7e5e44e2beedc4a73d70
75asa <75asa.me@gmail.com>
2022-01-31 23:56:09 +0900

    :sparkles: new bolt core module

---
+++ src/bolt/bolt.controller.ts
@@ -0,0 +1,11 @@
+import { Controller } from '@nestjs/common';
+import { BoltService } from './bolt.service';
+
+@Controller('bolt')
+export class BoltController {
+  constructor(private boltService: BoltService) {}
+
+  async run() {
+    await this.boltService.run();
+  }
+}
