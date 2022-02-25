3100354bc5acb6c9f0f43aa449be2e03d9934bae
75asa <75asa.me@gmail.com>
2022-02-26 00:23:17 +0900

    🚨 allow no-empty-constructor

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,6 +20,7 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-namespace': 'off',
+    '@typescript-eslint/no-empty-function': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
   },
 };
